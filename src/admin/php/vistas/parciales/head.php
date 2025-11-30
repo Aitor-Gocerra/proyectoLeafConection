8b81da7dfb2bbc3f0f35b4a25155e58cb0e01b21
Aitor-Gocerra <aitorgomezcerrato.guadalupe@alumnado.fundacionloyola.net>
2025-11-30 19:47:30 +0100

    Add admin styles for news and user panels
    Introduced new CSS rules in estilosAdmin.css for news and user management panels, including responsive layouts and form styling. Updated head.php to load estilosAdmin.css instead of estilo.css to apply the new admin styles.

--- src/admin/php/vistas/parciales/head.php
+++ src/admin/php/vistas/parciales/head.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
     function encabezado($titulo){
         echo ' 
 
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>'. $titulo.'</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Google+Sans+Code:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-            <link rel="stylesheet" href="css/estilo.css"> 
-            
-            ';
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>' . $titulo . '</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?family=Google+Sans+Code:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+                <link rel="stylesheet" href="css/estilosAdmin.css"> 
+                
+                ';
     }
 ?>
