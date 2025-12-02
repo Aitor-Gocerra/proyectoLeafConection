9a3a9d722b9253aa0aae9a7b753f076163e2a6e6
Adrián Zambrano <adrianzambranolopez.guadalupe@alumnado.fundacionloyola.net>
2025-12-02 12:42:17 +0100

    a
    a

--- src/game/php/vistas/gestionAmigos.php
+++ src/game/php/vistas/gestionAmigos.php
@@ -67,7 +67,35 @@
         require_once 'parciales/footer.php';
         ?>
     </footer>
-    <script src="javaScript/amigosSolicitudes.js"></script>
+    <script >
+        const btnAmigos = document.getElementById("todosAmigos");
+        const btnSolicitudes = document.getElementById("solicitudesAmigos");
+        const misAmigos = document.getElementById("misAmigos");
+        const misSolicitudes = document.getElementById("misSolicitudes");
+
+        btnAmigos.addEventListener("click", () => {
+            misAmigos.style.display = "block";
+            misSolicitudes.style.display = "none";
+
+            btnAmigos.classList.add("botonActivo");
+            btnAmigos.classList.remove("botonNoActivo");
+
+            btnSolicitudes.classList.add("botonNoActivo");
+            btnSolicitudes.classList.remove("botonActivo");
+        });
+
+        btnSolicitudes.addEventListener("click", () => {
+            misAmigos.style.display = "none";
+            misSolicitudes.style.display = "block";
+
+            btnSolicitudes.classList.add("botonActivo");
+            btnSolicitudes.classList.remove("botonNoActivo");
+
+            btnAmigos.classList.add("botonNoActivo");
+            btnAmigos.classList.remove("botonActivo");
+        });
+
+    </script>
 </body>
 
 </html>
