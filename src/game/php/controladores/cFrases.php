a10cfcac282c3a612085649c8d7fd3043c9e4dfc
Aitor-Gocerra <aitorgomezcerrato.guadalupe@alumnado.fundacionloyola.net>
2025-12-03 17:43:46 +0100

    Añado el controlador y el modelo para el juego de Frases

---
+++ src/game/php/controladores/cFrases.php
@@ -0,0 +1,49 @@
+<?php
+require_once __DIR__ . '/../modelos/mFrase.php';
+
+class CFrases
+{
+
+    private $fraseMod;
+    public $mensaje;
+    public $frase;
+    public $pista;
+    public $vista;
+
+    public function __construct()
+    {
+
+        $this->fraseMod = new Frase();
+        $this->vista = '';
+    }    
+    public function cargaDatosJuego()
+    {
+
+        $this->vista = 'fraseDia';
+
+        $this->frase = $this->fraseMod->mostrarFrase();
+
+        // Verificamos si realmente se encontró una frase para hoy (si no es false)
+        if ($this->frase) {
+
+            // Extraemos el ID del array que nos devolvió el modelo
+            $idFrase = $this->frase['idFrase'];
+
+            // Ahora sí podemos pedir la pista usando ese ID
+            $this->pista = $this->fraseMod->mostrarPista($idFrase);
+
+        } else {
+            // Si no hay frase programada para hoy, dejamos todo vacío o ponemos un mensaje
+            $this->frase = null;
+            $this->pista = null;
+            $this->mensaje = "No hay frase programada para la fecha de hoy.";
+        }
+        return [
+            'frase' => $this->frase,
+            'pista' => $this->pista,
+            'mensaje' => $this->mensaje
+        ];
+
+    }
+}
+?>
