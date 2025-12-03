47d546ddf04661b1b1a7ea553d770098ce9f9194
Jaime Rodríguez Salamanca <jaimerodriguezsalamanca.guadalupe@alumnado.fundacionloyola.net>
2025-12-03 13:56:14 +0100

    Modificacion html, js y html (pista)

--- src/game/php/vistas/fraseDia.php
+++ src/game/php/vistas/fraseDia.php
@@ -12,21 +12,38 @@
                 require_once 'parciales/nav.php';
             ?>
         </header>
+        
         <main>
+            
             <div id="cajaPalabra">
                 <h2 class="tituloPalabra">Frase del día</h2>
+                
                 <p class="descripcionPalabra">Rellena el espacio en blanco para completar la cita ecologica.</p>
-                <p class="temporizador"><i class="fas fa-clock"></i> </i>5:00</p>
-                <div class="pista">
+                
+                <div class="contenedorTiempoPista">
+                <p id="temporizador"><i class="fas fa-clock"></i> </i>5:00</p>
+                
+            <i class="fas fa-lightbulb fa-2x" id="iconoPista"></i> 
+            </div>
+            <div id="popupPista" class="popup">
+            <div class="popup-contenido">
+        <span id="cerrarPopup" class="cerrar"></span>
+        <p>Esta pista te ayudará a descubrir la eco-frase del día.</p>
+            </div>
+            </div>
+
+                <div class="frase">
                     <p>"La mayor amenaza para nuestro planeta es la creencia de que alguien mas lo ______".</p>
                 </div>
-                    <div class="contenedorAcierto">
-                        <input type="text" name="acertarPalabra" class="introducirPalabra" placeholder="Tu suposición...">
-                        <button type="submit" class="enviarPalabra">Enviar</button>
-                    </div>
-            </div>
-        </main>
         
+        
+
+        <div class="contenedorAcierto">
+            <input type="text" name="acertarPalabra" class="introducirPalabra" placeholder="Tu suposición...">
+            <button type="submit" class="enviarPalabra">Acierta</button>
+        </div>
+    </div>
+    </main>
         <?php
              require_once 'parciales/botonVolver.php';
         ?>
@@ -37,7 +54,32 @@
                 require_once 'parciales/footer.php';
             ?>
         </footer>
+        <script>
+            document.addEventListener('DOMContentLoaded', () => {
+    
+    const iconoPista = document.querySelector('.iconoPista');
+    const popup = document.getElementById('popupPista');
+    const cerrarPopup = document.getElementById('cerrarPopup');
 
-        <script src="../../javascript/menuDesplegable.js"></script>
+    
+    iconoPista.addEventListener('click', () => {
+        popup.style.display = 'block';
+    });
+
+    
+    cerrarPopup.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+
+    
+    window.addEventListener('click', (event) => {
+        
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+        </script>
+    
     </body>
 </html>
--- src/game/php/vistas/palabraDia.php
+++ src/game/php/vistas/palabraDia.php
@@ -5,7 +5,9 @@
             require_once 'parciales/head.php';
             encabezado("Palabra del día - LeafConnect");
         ?>
+        
     </head>
+    
     <body>
         <header>
             <?php
@@ -16,10 +18,25 @@
             <div id="cajaPalabra">
                 <h2 class="tituloPalabra">Palabra del día</h2>
                 <p class="descripcionPalabra">Usa la siguiente definición para adivinar la eco-palabra.</p>
-                <p class="temporizador"><i class="fas fa-clock"></i> </i>5:00</p>
-                <div class="pista">
+                
+                <div class="contenedorTiempoPista">
+                <p id="temporizador"><i class="fas fa-clock"></i> </i>5:00</p>
+                
+            <i class="fas fa-lightbulb fa-2x" id="iconoPista"></i> 
+            </div>
+            <div id="popupPista" class="popup">
+            <div class="popup-contenido">
+        <span id="cerrarPopup" class="cerrar"></span>
+        <p>Esta pista te ayudará a descubrir la eco-frase del día.</p>
+            </div>
+            </div>
+            
+        
+                
+                <div class="frase">
                     <p>El proceso de reciclaje de materia orgánica, como hojas y restos de comida, en una enmienda rica para el suelo.</p>
                 </div>
+
                     <div class="contenedorAcierto">
                         <input type="text" name="acertarPalabra" class="introducirPalabra" placeholder="Tu suposición...">
                         <button type="submit" class="enviarPalabra">Acierta</button>
@@ -34,5 +51,22 @@
                 require_once 'parciales/footer.php';
             ?>
         </footer>
+        <script>
+            document.querySelector('#iconoPista').addEventListener('click', function() {
+            document.getElementById('popupPista').style.display = 'block';
+    });
+
+            document.getElementById('cerrarPopup').addEventListener('click', function() {
+            document.getElementById('popupPista').style.display = 'none';
+});
+
+        // Cerrar si hace clic fuera del popup
+        window.addEventListener('click', function(ventana) {
+        let popup = document.getElementById('popupPista');
+        if (ventana.target === popup) {
+        popup.style.display = 'none';
+    }
+});
+        </script>
     </body>
 </html>
