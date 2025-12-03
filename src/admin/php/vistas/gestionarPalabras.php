b1384edeb4162f858cf7cf4c1c766a6c0079d55b
Aitor Gomez Cerrato <aitorgomezcerrato@gmail.com>
2025-12-03 10:32:16 +0100

    Update gestionarPalabras.php

--- src/admin/php/vistas/gestionarPalabras.php
+++ src/admin/php/vistas/gestionarPalabras.php
@@ -82,7 +82,7 @@
                 <div id="cuestionarioContainer">
                     <div class="cuestionarioPregunta">
                         <label>Pista</label>
-                        <input type="text" name="pista[]" placeholder="Pista...">
+                        <input type="text" name="pista[]" placeholder="Pista..." required>
                     </div>
                 </div>
 
