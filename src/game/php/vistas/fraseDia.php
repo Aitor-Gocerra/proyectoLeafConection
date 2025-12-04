f8a7e3ca4cae207ea7f4c7c9a6869eb302886a51
Aitor Gomez Cerrato <aitorgomezcerrato@gmail.com>
2025-12-04 10:53:30 +0100

    Update fraseDia.php

--- src/game/php/vistas/fraseDia.php
+++ src/game/php/vistas/fraseDia.php
@@ -16,14 +16,19 @@
             <div id="cajaPalabra">
                 <h2 class="tituloPalabra">Frase del día</h2>
                 <p class="descripcionPalabra">Rellena el espacio en blanco para completar la cita ecologica.</p>
-                <p class="temporizador"><i class="fas fa-clock"></i> </i>5:00</p>
-                <div class="pista">
-                    <p>"La mayor amenaza para nuestro planeta es la creencia de que alguien mas lo ______".</p>
+                <div class="contenedor-timer-pista">
+                    <p class="temporizador"><i class="fas fa-clock"></i> 5:00</p>
+                    <button id="btnPista" class="boton-bombilla" title="Ver pista extra">
+                        <i class="fas fa-lightbulb"></i>
+                    </button>
                 </div>
-                    <div class="contenedorAcierto">
-                        <input type="text" name="acertarPalabra" class="introducirPalabra" placeholder="Tu suposición...">
-                        <button type="submit" class="enviarPalabra">Enviar</button>
-                    </div>
+                <div class="pista">
+                    <p><?php echo isset($frase['definicion']) ? $frase['definicion'] : 'No hay palabra disponible.'; ?></p>
+                </div>
+                <div class="contenedorAcierto">
+                    <input type="text" name="acertarPalabra" class="introducirPalabra" placeholder="Tu suposición...">
+                    <button type="submit" class="enviarPalabra">Enviar</button>
+                </div>
             </div>
 
             <?php
