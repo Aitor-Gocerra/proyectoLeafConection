96923460ea3d5cd0af66087cfea492ca0da5e745
Aitor Gomez Cerrato <aitorgomezcerrato@gmail.com>
2025-12-06 15:10:00 +0100

    Cambios en los datos de acceso a la BD para el hosting

--- src/admin/php/modelos/configDB.php
+++ src/admin/php/modelos/configDB.php
@@ -1,6 +1,6 @@
 <?php
-    DEFINE ("servidor", "localhost");
-    DEFINE ("usuario", "root");
-    DEFINE ("password", "");
-    DEFINE ("nombreDB", "leafconnect");
+    DEFINE ("servidor", "21.daw.esvirgua.com");
+    DEFINE ("usuario", "daw_userbd21");
+    DEFINE ("password", "21NTiJq]K)7]SQB=");
+    DEFINE ("nombreDB", "daw_21_BD1");
 ?>
