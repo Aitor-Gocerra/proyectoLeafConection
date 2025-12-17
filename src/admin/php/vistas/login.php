317a2719806c4a32047abd128b26a343363fd762
Adrián Zambrano <adrianzambranolopez.guadalupe@alumnado.fundacionloyola.net>
2025-12-17 13:52:24 +0100

    Create login.php

---
+++ src/admin/php/vistas/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <?php
+            require_once 'parciales/head.php';
+            encabezado("Login - LeafConnect");
+        ?>
+</head>
+
+<body>
+    <header>
+        <nav class="navegacion-autenticacion">
+            <div class="infoJuego">
+                <img src="./imagenes/logo.jpg" alt="logo Juego" class="logo">
+                <h3>LeafConnect</h3>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+       <form action="" id="form-login" class="formulario-autenticacion"> 
+            <p>Introduce tus datos para acceder al admin.</p>
+            
+            <label for="input-email">Correo electrónico</label>
+            <input type="text" id="input-email" placeholder="Introduce correo"> 
+            
+            <div class="info-password-label">
+                <label for="input-password" id="tituloPw">Contraseña</label>
+                <span id="iconoPw">🔒</span> 
+            </div>
+            <input type="password" id="input-password" placeholder="Introduce contraseña">
+            
+
+            <div id="mensaje-error"></div>
+            
+            <button type="button" id="btn-login" class="boton-autenticacion-primario">Iniciar Sesión</button>
+        </form>
+
+    </main>
+
+    <footer>
+        <?php
+        require_once 'parciales/footer.php';
+        ?>
+    </footer>
+    <script src="javaScript/modelos/mIniciarsesion.js"></script>
+    <script src="javaScript/controladores/cIniciarsesion.js"></script>
+    <script src="javaScript/vistas/vIniciarsesion.js"></script>
+    <script src="javaScript/app.js"></script>
+</body>
+
+</html>
